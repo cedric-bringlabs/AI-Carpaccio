46a77c1487ce9cd8574b0a57cdc0910c2df1a8d6
Cedric 🐈 <cedric.buehler@getbring.com>
2026-03-25 13:41:33 +0100

    Add ignored test list for virtual pet stage

---
+++ src/test/kotlin/org/example/pet/VirtualPetTest.kt
@@ -0,0 +1,43 @@
+package org.example.pet
+
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+class VirtualPetTest {
+
+    @Ignore
+    @Test
+    fun starts_with_a_name_and_zero_hunger() {
+        TODO("Test to be implemented in a later commit.")
+    }
+
+    @Ignore
+    @Test
+    fun feeding_reduces_hunger_by_five() {
+        TODO("Test to be implemented in a later commit.")
+    }
+
+    @Ignore
+    @Test
+    fun feeding_does_not_reduce_hunger_below_zero() {
+        TODO("Test to be implemented in a later commit.")
+    }
+
+    @Ignore
+    @Test
+    fun tick_increases_hunger_by_one() {
+        TODO("Test to be implemented in a later commit.")
+    }
+
+    @Ignore
+    @Test
+    fun hunger_does_not_exceed_the_maximum() {
+        TODO("Test to be implemented in a later commit.")
+    }
+
+    @Ignore
+    @Test
+    fun status_reports_name_and_hunger() {
+        TODO("Test to be implemented in a later commit.")
+    }
+}
